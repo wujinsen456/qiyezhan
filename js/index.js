5a3ebae18b9c2d01defbea7f9601ca1be5eb30e2
wujinsen456 <1098206859@qq.com>
2018-09-11 13:33:18 +0800

    wjs

--- js/index.js
+++ js/index.js
@@ -6,9 +6,7 @@ window.onload=function () {
 
     /////////搜索框/////////////////////////
     let search_all=document.querySelector(".search_all");
-    console.log(search_all);
     let icon_se=document.querySelector(".icon_se");
-    console.log(icon_se);
     icon_se.onclick=function () {
         search_all.style.display="block";
     };
@@ -16,7 +14,6 @@ window.onload=function () {
     let plantId=document.getElementById("plant");
     let search_button=document.querySelector(".search_button");
     let search_button1=document.querySelector(".search_button1");
-    console.log(search_button, search_button1, plantId);
     search_button.onclick=function () {
         if(plantId.value=="植物花卉001"||plantId.value=="001"){
             open("xiangqing1.html");
@@ -132,6 +129,120 @@ window.onload=function () {
         }
     });
 
+
+    //////////banner_down//////////
+    let banner_down=document.querySelector(".banner_down");
+    let banner_down_TOP=banner_down.offsetTop;
+
+    let photo1_size1=document.querySelector(".photo1_size1");
+    let photo1_size2=document.querySelector(".photo1_size2");
+    ///////////today////////////////
+    let todayLi=document.querySelectorAll(".today");
+    let arr=[];
+    todayLi.forEach((v)=>{
+        arr.push(v.offsetTop);
+    });
+    let today_allLi=document.querySelectorAll(".today_all");
+    /////////////today_display///////////////
+    let today_displayLi=document.querySelector(".today_display");
+    let rowLi_TOP=today_displayLi.offsetTop;
+    let Display=document.querySelector(".container_display li");
+    ////////////限时促销下面////////////
+
+    let promotion=document.querySelector(".promotion");
+    let promotion_TOP=promotion.offsetTop;
+
+    let promotion_left=document.querySelector(".promotion_left");
+    let promotion_right=document.querySelector(".promotion_right");
+
+    ////////////////introduce///////////
+    let introduce=document.querySelector(".introduce");
+    let introduce_TOP=introduce.offsetTop;
+
+    let bac1_p=document.querySelector(".bac1_p");
+
+    //////////decorate//////////////////
+    let decorate=document.querySelector(".decorate");
+    let decorate_TOP=decorate.offsetTop;
+
+    let col_dec_allLi=document.querySelectorAll(".col_dec_all");
+    console.log(col_dec_allLi);
+    ////////////////translate/////////////
+    let translate=document.querySelector(".translate");
+    let translate_TOP=translate.offsetTop;
+
+    let col_trans=document.querySelector(".col_trans");
+    let trans3_size=document.querySelector(".trans3_size");
+    //////////////iconall//////////////////
+    let iconall=document.querySelector(".iconall");
+    let iconall_TOP=iconall.offsetTop;
+
+    let iconallLi=document.querySelectorAll(".iconallLi");
+/////////////btn///////////////////
+    let icon=document.querySelector(".icon-zhiding");
+    let wh=window.innerHeight;
+        window.onscroll=function() {
+            let bh=document.body.scrollTop||document.documentElement.scrollTop;
+            if(wh+bh>=banner_down_TOP+50){
+                photo1_size1.style.display="block";
+                photo1_size2.style.display="block";
+                photo1_size1.style.animation="photo1_size1 1.5s 1 forwards";
+                photo1_size2.style.animation="photo1_size2 1.5s 1 forwards";
+            }
+            arr.forEach((e,i)=>{
+                if(wh+bh>=e+10){
+                    today_allLi.forEach(()=>{
+                        today_allLi[i].style.display="block";
+                        today_allLi[i].style.animation="container_display 1s 1 forwards";
+                    })
+
+                }
+            });
+            if(wh+bh>=rowLi_TOP+100){
+                Display.style.display="block";
+                Display.style.animation="container_display 1.5s 1 forwards";
+            }
+            if(wh+bh>=promotion_TOP+20){
+                promotion_left.style.display="block";
+                promotion_right.style.display="block";
+                promotion_left.style.animation="photo1_size1 1.5s 1 forwards";
+                promotion_right.style.animation="photo1_size2 1.5s 1 forwards";
+            }
+            if(wh+bh>=introduce_TOP+100){
+                bac1_p.style.display="block";
+                bac1_p.style.animation="photo1_size2 1.7s 1 forwards";
+            }
+            if(wh+bh>=decorate_TOP+20){
+                col_dec_allLi.forEach((e)=>{
+                    e.style.display="block";
+                    col_dec_allLi[0].style.animation="container_display 1.5s 1 forwards";
+                    col_dec_allLi[1].style.animation="container_display 1.7s 1 forwards";
+                    col_dec_allLi[2].style.animation="container_display 1.9s 1 forwards";
+                    col_dec_allLi[3].style.animation="container_display 2.1s 1 forwards";
+                })
+            }
+            if(wh+bh>=translate_TOP+20){
+                col_trans.style.display="block";
+                trans3_size.style.display="block";
+                col_trans.style.animation="photo1_size1 1.5s 1 forwards";
+                trans3_size.style.animation="photo1_size2 1.5s 1 forwards";
+            }
+            if(wh+bh>=iconall_TOP){
+                iconallLi.forEach((e)=>{
+                    e.style.display="block";
+                    iconallLi[0].style.animation="container_display 1.1s 1 forwards";
+                    iconallLi[1].style.animation="container_display 1.3s 1 forwards";
+                    iconallLi[2].style.animation="container_display 1.5s 1 forwards";
+                    iconallLi[3].style.animation="container_display 1.7s 1 forwards";
+                })
+
+            }
+            icon.onclick=function () {
+                animate(document.body,{scrollTop:0});
+                animate(document.documentElement,{scrollTop:0})
+            };
+
+        };
 
 
 };
